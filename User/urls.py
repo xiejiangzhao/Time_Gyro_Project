50c86ab2b70b9ecbf08f46a3b3e3b81a65404ca5
谢江钊 <10751583@qq.com>
2018-06-25 17:14:53 +0800

    前端信息更新

--- User/urls.py
+++ User/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login_view, name='login'),
     path('test/', views.test, name='test'),
+    path('logout/', views.login_out, name='logout'),
     path('register/', views.register, name='register'),
     path('<str:username>/', views.userprofile, name='userprofile'),
     path('<str:username>/create_schedule/', views.create_schedule, name='create_schedule')
